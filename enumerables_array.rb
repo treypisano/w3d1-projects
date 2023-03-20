2904d6dbb7822e8215c88e549bf5ec509e8ac730
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-03-20 15:32:48 -0400

    Push enumerables

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -11,20 +11,38 @@ class Array
         self
     end
 
+    def my_select(&blk)
+        new_arr = []
+        self.my_each do |ele|
+            new_arr << ele if blk.call(ele)
+        end
+        return new_arr
+    end
+
+    def my_reject(&blk)
+        self - self.my_select{|ele| ele if blk.call(ele)}
+    end
 
 end
 
 # calls my_each twice on the array, printing all the numbers twice.
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-   end.my_each do |num|
-    puts num
-   end
-   # => 1
-   #    2
-   #    3
-   #    1
-   #    2
-   #    3
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#    end.my_each do |num|
+#     puts num
+#    end
+#    # => 1
+#    #    2
+#    #    3
+#    #    1
+#    #    2
+#    #    3
    
-   p return_value  # => [1, 2, 3]
+#    p return_value  # => [1, 2, 3]
+
+# a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+a = [1, 2, 3]
+p a.my_reject { |num| num > 1 } # => [1]
+p a.my_reject { |num| num == 4 } # => [1, 2, 3]
