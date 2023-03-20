4497223f66bd1c244646903c781e2ed16ac8415f
Angelo Ciffone <angelo.pcr@yahoo.com>
2023-03-20 16:31:30 -0400

    new changes

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -38,6 +38,38 @@ class Array
             return false
         end
     end
+
+    def my_flatten
+        if !self.is_a?(Array)
+            return [self]
+        end
+        flattened = []
+        self.each do |ele|
+            if ele.is_a?(Array)
+                flattened += ele.my_flatten
+            else 
+                flattened << ele
+            end
+        end
+        flattened
+    end
+
+    def my_zip(*args)
+        new_arr = []
+        self.each_with_index do |self_ele, i|
+            new_sub_arr = []
+            new_sub_arr << self_ele
+            args.each do |arg|
+                if i < arg.length 
+                    new_sub_arr << arg[i]
+                else
+                    new_sub_arr << nil
+                end
+            end
+            new_arr << new_sub_arr
+        end
+        new_arr
+    end
 end
 
 # calls my_each twice on the array, printing all the numbers twice.
@@ -62,8 +94,20 @@ end
 # p a.my_reject { |num| num > 1 } # => [1]
 # p a.my_reject { |num| num == 4 } # => [1, 2, 3]
 
-a = [1, 2, 3]
-p a.my_any? { |num| num > 1 } # => true
-p a.my_any? { |num| num == 4 } # => false
-p a.my_all? { |num| num > 1 } # => false
-p a.my_all? { |num| num < 4 } # => true
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
