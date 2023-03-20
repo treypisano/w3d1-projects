6d9af560db551883a4be9b255f57964f8acfadc9
treypisano <treypisano@TreysPC>
2023-03-20 16:02:47 -0400

    Added any? and all

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -23,6 +23,21 @@ class Array
         self - self.my_select{|ele| ele if blk.call(ele)}
     end
 
+    def my_any?(&blk)
+        if self.my_select {|ele| blk.call(ele)}.length >= 1
+            return true
+        else
+            return false
+        end
+    end
+
+    def my_all?(&blk)
+        if self.my_select {|ele| blk.call(ele)}.length == self.length
+            return true
+        else
+            return false
+        end
+    end
 end
 
 # calls my_each twice on the array, printing all the numbers twice.
@@ -43,6 +58,12 @@ end
 # a = [1, 2, 3]
 # p a.my_select { |num| num > 1 } # => [2, 3]
 # p a.my_select { |num| num == 4 } # => []
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
 a = [1, 2, 3]
-p a.my_reject { |num| num > 1 } # => [1]
-p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
